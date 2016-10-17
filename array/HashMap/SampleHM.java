7c361ed752da2a047d7a6ddb9dd4a3a0eb68fbb3
SanthiyaJ <santhiyajayabalan@gmail.com>
2016-10-16 23:04:34 -0500

    Hashmap sample

---
+++ array/HashMap/SampleHM.java
@@ -0,0 +1,35 @@
+package HashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+public class SampleHM {
+	public static void main(String[] args) {
+		/*//Syntax for hash map
+		HashMap<Integer, String> hm=new HashMap<Integer ,String>();
+		//Entering values in the hash map
+		hm.put(1,"apple");
+		hm.put(2, "balls");
+		hm.put(3, "Cats");
+		//implementing various possibility in the hash map functionality
+		System.out.println(hm.get("3"));
+		System.out.println(hm.containsKey(1));
+		
+		ArrayList[] al = new ArrayList[5];
+		al[0].add(1);
+		al[1].add(2);
+		al[2].add(3);
+		al[3].add(4);
+		al[4].add(5);
+		al[5].add(6);
+		*/
+		int a =4;
+		short b = a;
+		//int c = b;
+		System.out.println(b);
+		
+	}
+
+}
---
+++ array/Sample1.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.*;
+public class Sample1 {
+	
+	public static void main(String args[]) {
+		
+	int[] arr1= new int[5];
+	
+			arr1[0]=1;
+			arr1[1]=2;
+			arr1[2]=3;
+			arr1[3]=4;
+			arr1[4]=5;
+			
+			System.out.println("Array elemnets in the list are as below");
+			for(int i=0; i<=arr1.length;i++)
+			{
+				System.out.println(i);
+			}
+	}
+	
+
+}
